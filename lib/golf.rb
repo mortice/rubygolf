240150bcc106f591e336ada9fcd5d9f5fea54165
Tom Stuart <tom@therye.org>
2011-05-09 19:10:52 +0100

    Not fully working hole 5

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,14 @@
 class Golf
+  class << self
+		def hole5(a)
+			final = []
+			a.each_with_index do |i, index|
+				final << [i]
+				1.upto(3) do |i|
+					final << a[index..(index+i)] unless index + i >= a.size
+				end
+			end
+			final.sort {|a, b| a[0] <=> b[0]}.sort {|a, b| a.size <=> b.size}
+		end
+	end	
 end
