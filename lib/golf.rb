2bd1fae14de7390223d295709bb4c639b5e09685
Jamie Mill <jamiermill@gmail.com>
2011-05-09 19:41:40 +0100

    remove self.

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,38 +1,38 @@
 class Golf
 
-  def self.hole1 a
-    eval a.join '*'
-  end
-
-  def self.hole3 n
-    eval (1..n).to_a.join '*'
-  end
-
-  def self.hole6 t
-    a = []
-    (1..t).each do |i|
-      if i%15 == 0
-        a << 'fizzbuzz'
-      elsif i%3==0
-        a << 'fizz'
-      elsif i%5==0
-        a << 'buzz'
-      else
-        a << i
-      end
-    end
-    a
-  end
-
-  def self.hole8 t
-    a = [1,1]
-    (t-2).times do |i|
-      a << (a[i+1] + a[i])
-    end
-    a
-  end
-
   class << self
+    def hole1 a
+      eval a.join '*'
+    end
+
+    def hole3 n
+      eval (1..n).to_a.join '*'
+    end
+
+    def hole6 t
+      a = []
+      (1..t).each do |i|
+        if i%15 == 0
+          a << 'fizzbuzz'
+        elsif i%3==0
+          a << 'fizz'
+        elsif i%5==0
+          a << 'buzz'
+        else
+          a << i
+        end
+      end
+      a
+    end
+
+    def hole8 t
+      a = [1,1]
+      (t-2).times do |i|
+        a << (a[i+1] + a[i])
+      end
+      a
+    end
+
 		def hole5(a)
 			f = []
 			0.upto(a.size-1) do |j|
