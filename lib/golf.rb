6666f65a1f83ad18d1dd5b24204cba8edc4be74d
Jamie Mill <jamiermill@gmail.com>
2011-05-09 19:28:42 +0100

    some answers

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,35 @@
 class Golf
+
+  def self.hole1 a
+    eval a.join '*'
+  end
+
+  def self.hole3 n
+    eval (1..n).to_a.join('*')
+  end
+
+  def self.hole6 t
+    a = []
+    (1..t).to_a.each do |i|
+      if i%3==0 && i%5==0
+        a << 'fizzbuzz'
+      elsif i%3==0
+        a << 'fizz'
+      elsif i%5==0
+        a << 'buzz'
+      else
+        a << i
+      end
+    end
+    a
+  end
+
+  def self.hole8 t
+    a = [1,1]
+    (1..t-2).each do |i|
+      a << (a[i] + a[i-1])
+    end
+    a
+  end
+
 end
